f54e89542c4926cad4148e76ee510d6bee0ad855
81jv <elinalazarevamilana@gmail.com>
2026-04-18 00:05:54 +0900

    Create converter_mp3.py

---
+++ converter_mp3.py
@@ -0,0 +1,25 @@
+import yt_dlp
+import os
+
+def converter_mp3(url, save_path='./Downloads'): # Auto-Create folder with your name  
+    os.makedirs(save_path, exist_ok=True)
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'ffmpeg_location': r'', # Location ffmpeg on OS
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        'outtmpl': os.path.join(save_path, '%(title)s.%(ext)s'),
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+if __name__ == '__main__':
+    link = input('Link: ').strip()
+    converter_mp3(link)
+  
+# So...first step you need download ffmpeg, zip file example: ffmpeg-8.1-essentials_build
